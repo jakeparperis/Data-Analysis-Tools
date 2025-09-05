66135308832e29ed7f6f119e1379f37283779314
jakep <jakeparperis@gmail.com>
2025-09-05 20:59:18 +0100

    Manually input dataset for 24/25 epl fullback ball progression

---
+++ src/hardcoded_data_sets.py
@@ -0,0 +1,21 @@
+# Non-Relegated
+prem_fbs_names_2425 = ["Djed Spence", "Pedro Porro", "Destiny Udogie", "Alexander-Arnold", "Andrew Robertson",
+               "Kostas Tsimikas", "Conor Bradley", "Ben White", "Myles Lewis-Skelly", "Oleksandr Zinchenko",
+               "Riccardo Calafiori", "Jurriën Timber", "Joško Gvardiol", "Rico Lewis", "Kyle Walker", "Marc Cucurella",
+               "Malo Gusto", "Reece James", "Tino Livramento", "Kieran Trippier", "Matty Cash", "Lucas Digne",
+               "Ian Maatsen", "Ola Aina", "Neco Williams", "Alex Moreno", "Pervis Estupiñán", "Joël Veltman",
+               "Tariq Lamptey", "Milos Kerkez", "Adam Smith", "Keane Lewis-Potter", "Michael Kayode", "Mads Roerslev",
+               "Kristoffer Ajer", "Antonee Robinson", "Kenny Tete", "Timothy Castagne", "Ryan Sessegnon",
+               "Tyrick Mitchell", "Daniel Munoz", "Vitaliy Mykolenko", "Ashley Young", "Wan-Bissaka",
+               "Vladimir Coufal", "Emerson Palmieri", "Patrick Dorgu", "Diogo Dalot", "Noussair Mazraoui",
+               "Rayan Aït-Nouri", "Nélson Semedo", "Matt Doherty"]
+
+prem_fbs_prgp_2425 = [5.23, 5, 5.2, 8.41, 6.49, 6.71, 3.71, 5.06, 4.32, 8.27, 4.23, 5.55, 7.3, 6.46, 5, 3.65, 6.37,
+                      5.68, 5.26, 7.91, 2.07, 2.48, 4.09, 2.97, 3.47, 3.8, 4.65, 4.8, 1.47, 4.14, 2.84, 2.71, 2.7, 4.24,
+                      3.52, 3.94, 3.35, 3.29, 3.44, 1.89, 2.68, 3.35, 4.13, 4.28, 3.26, 2.85, 3.11, 3.92, 4.35, 3.05, 2.53,
+                      3.44]
+
+prem_fbs_prgc_2425 = [3.22, 2.1, 3, 1.64, 2.3, 1.36, 3.71, 1.89, 2.05, 1.4, 2.03, 1.99, 2.59, 1.93, 1.12, 1.06, 2.38,
+                      0.77, 2.73, 1.74, 1.94, 1.07, 2.27, 1.81, 2.43, 2.48, 2.07, 1.58, 3.35, 2.73, 0.79, 2.79, 1.6,
+                      1.62, 1.69, 3.57, 1.67, 0.91, 2.8, 2.04, 1.73, 0.96, 1.19, 3.05, 2.25, 1.98, 3.61, 2.25, 1.49, 2.73,
+                      2.11, 1.2]
